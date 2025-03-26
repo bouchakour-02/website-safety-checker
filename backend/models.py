98afb8a5b58301859729b7aaaa63ec968aeba181
bouchakour alaa <85706955+bouchakour-02@users.noreply.github.com>
2025-03-26 17:03:01 +0100

    Auto commit: 2025-03-26 17:03:01

--- backend/models.py
+++ backend/models.py
@@ -1,13 +1,13 @@
-from app import db
+# models.py
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+from extensions import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
-    # Additional fields (like subscription tier) can be added
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -18,6 +18,6 @@ class User(db.Model):
 class ScanHistory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     url = db.Column(db.String(2083), nullable=False)
-    result = db.Column(db.Text, nullable=False)  # Could be JSON stringified results
+    result = db.Column(db.Text, nullable=False)
     scanned_at = db.Column(db.DateTime, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
