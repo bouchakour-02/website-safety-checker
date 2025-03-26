33223d9cac4a592d815aa2832db96c82c77e9d2a
bouchakour alaa <85706955+bouchakour-02@users.noreply.github.com>
2025-03-26 16:42:27 +0100

    Auto commit: 2025-03-26 16:42:27

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 const Navbar = () => {
   return (
-    <nav style={{ padding: '1rem', background: '#f5f5f5' }}>
-      <Link to="/" style={{ marginRight: '1rem' }}>Home</Link>
-      <Link to="/scan" style={{ marginRight: '1rem' }}>Scan URL</Link>
-      <Link to="/login" style={{ marginRight: '1rem' }}>Login</Link>
-      <Link to="/register">Register</Link>
-    </nav>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" style={{ flexGrow: 1 }}>
+          Safety Checker
+        </Typography>
+        <Button color="inherit" component={Link} to="/">Home</Button>
+        <Button color="inherit" component={Link} to="/scan">Scan URL</Button>
+        <Button color="inherit" component={Link} to="/login">Login</Button>
+        <Button color="inherit" component={Link} to="/register">Register</Button>
+      </Toolbar>
+    </AppBar>
   );
 };
 
