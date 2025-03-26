62b407dae810e0a68b0e6c6ad43e825bcb3d8892
bouchakour alaa <85706955+bouchakour-02@users.noreply.github.com>
2025-03-26 16:42:17 +0100

    Auto commit: 2025-03-26 16:42:17

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,64 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence, motion } from 'framer-motion';
 import Navbar from './components/Navbar';
 import LoginForm from './components/LoginForm';
 import RegisterForm from './components/RegisterForm';
 import ScanForm from './components/ScanForm';
 
+const pageVariants = {
+  initial: {
+    opacity: 0,
+    y: 20,
+  },
+  in: {
+    opacity: 1,
+    y: 0,
+  },
+  out: {
+    opacity: 0,
+    y: -20,
+  },
+};
+
+const pageTransition = {
+  type: "tween",
+  ease: "anticipate",
+  duration: 0.5,
+};
+
+const AnimatedRoute = ({ children }) => (
+  <motion.div
+    initial="initial"
+    animate="in"
+    exit="out"
+    variants={pageVariants}
+    transition={pageTransition}
+    style={{ height: "100%" }}
+  >
+    {children}
+  </motion.div>
+);
+
+function AnimatedRoutes() {
+  const location = useLocation();
+  return (
+    <AnimatePresence exitBeforeEnter>
+      <Routes location={location} key={location.pathname}>
+        <Route path="/login" element={<AnimatedRoute><LoginForm /></AnimatedRoute>} />
+        <Route path="/register" element={<AnimatedRoute><RegisterForm /></AnimatedRoute>} />
+        <Route path="/scan" element={<AnimatedRoute><ScanForm /></AnimatedRoute>} />
+        <Route path="/" element={<AnimatedRoute><ScanForm /></AnimatedRoute>} />
+      </Routes>
+    </AnimatePresence>
+  );
+}
+
 function App() {
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/register" element={<RegisterForm />} />
-        <Route path="/scan" element={<ScanForm />} />
-        <Route path="/" element={<ScanForm />} />
-      </Routes>
+      <AnimatedRoutes />
     </Router>
   );
 }
