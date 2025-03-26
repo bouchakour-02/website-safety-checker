525bcceb51cf441aa5cb4a5b29d62fe1d52a12be
bouchakour alaa <85706955+bouchakour-02@users.noreply.github.com>
2025-03-26 16:18:25 +0100

    Auto commit: 2025-03-26 16:18:25

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
     baseURL: process.env.REACT_APP_API_URL ||'https://localhost:5000',
 });
 
-api.interceptors.request.use((config)=>{}
+api.interceptors.request.use((config)=>{
 const token = localStorage.getItem('access_token');
 if (token) {
     config.headers['Authorization'] = `Bearer ${token}`;
