c5ab205e240a1255e1d940fa30d5fc4dd8d1558d
bouchakour alaa <85706955+bouchakour-02@users.noreply.github.com>
2025-03-26 16:55:53 +0100

    Auto commit: 2025-03-26 16:55:53

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios' ; 
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL ||'https://127.0.0.1:5000:5000',
+    baseURL: process.env.REACT_APP_API_URL ||'https://127.0.0.1:5000',
 });
 
 api.interceptors.request.use((config)=>{
