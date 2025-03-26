6bb53f9c456229f6e05ba3872cc3e5fa5335ade6
bouchakour alaa <85706955+bouchakour-02@users.noreply.github.com>
2025-03-26 16:58:58 +0100

    Auto commit: 2025-03-26 16:58:58

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from config import Config
-
+from routes import main_bp
 db = SQLAlchemy()
 
 def create_app():
